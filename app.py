996fc6fa8a4fe909e96cbbe8fddfde4f05a9391f
Matthew Upson <matthew.a.upson@gmail.com>
2018-03-28 13:36:24 +0100

    Added input field for github/zenodo

--- app.py
+++ app.py
@@ -2,23 +2,31 @@
 Simple flask app
 """
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField
+from wtforms.validators import DataRequired
 app = Flask(__name__)
 
-
-UPLOAD_FOLDER = '/tmp/'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+class GetPapersQuery(FlaskForm):
+    name = SelectField(
+        'Select Query',
+        choices = [('github', 'github'),('zenodo', 'zenodo')],
+        validators=[DataRequired()])
 
 # Set the app secret key to prevent CSRF
 
 app.secret_key = os.urandom(24)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def root():
     """
     Root page of site
     """
-    return "Hello World"
+    form = GetPapersQuery()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
