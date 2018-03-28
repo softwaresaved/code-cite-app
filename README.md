c90a717ea507c2b506c4e5403d407e411b045226
Matthew Upson <matthew.a.upson@gmail.com>
2018-03-28 12:08:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# code-cite-app
+
+Web application built in Python (3.6.4) and flask for hosting the [code-cite](https://github.com/softwaresaved/code-cite) tool. 
---
+++ app.py
@@ -0,0 +1,25 @@
+"""
+Simple flask app
+"""
+import os
+from flask import Flask
+app = Flask(__name__)
+
+
+UPLOAD_FOLDER = '/tmp/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Set the app secret key to prevent CSRF
+
+app.secret_key = os.urandom(24)
+
+@app.route('/', methods=['POST'])
+def root():
+    """
+    Root page of site
+    """
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
